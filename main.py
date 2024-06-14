11f5186ac57c3538c591b463ba2e681dbc5db4c0
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-06-14 23:12:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,91 @@
-import socket as sk
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+import numpy as np
+import os
+from moviepy.editor import VideoFileClip
+import asyncio
+import websockets
+import socket
+import socket
 
-s = sk.gethostbyname(sk.gethostname())
-sock = sk.socket()
-sock.connect(("127.0.0.1", 1025))
+def get_local_ip_address():
+    # Créer une socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-to_send = open("moi.mp4", "rb").read()
-sock.send(to_send)
-recv = sock.recv(32).decode()
-print(recv)
+    try:
+        # Connexion à un serveur extérieur
+        s.connect(("8.8.8.8", 80))
+        local_ip_address = s.getsockname()[0]
+    finally:
+        s.close()
+
+    return local_ip_address
+
+def get_server_address(port):
+    local_ip_address = get_local_ip_address()
+    return f"ws://{local_ip_address}:{port}"
+
+# Port sur lequel votre serveur WebSocket écoute
+port = 8765
+
+# Obtenir l'adresse du serveur WebSocket
+server_address = get_server_address(port)
+print("Adresse du serveur WebSocket:", server_address)
+
+def decouper_video_en_images(video_path, output_folder):
+    # Assurez-vous que le dossier de sortie existe
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Charger la vidéo
+    video = VideoFileClip(video_path)
+
+    # Parcourir chaque trame de la vidéo
+    for i, frame in enumerate(video.iter_frames()):
+        # Enregistrer la trame sous forme d'image
+        frame_path = os.path.join(output_folder, f"frame_{i}.png")
+        video.save_frame(frame_path, t=i / video.fps)  # Utilisez save_frame() avec le temps de la trame
+        print(f"Enregistrement de la trame {i + 1}/{video.duration * video.fps}")
+
+    # Fermer la vidéo
+    video.close()
+
+def preprocess_image(file):
+    img = image.load_img(file, target_size=(150, 150))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+def verifie():
+    b, c = 0, 0
+    model = load_model("model.h5")
+    for i in os.listdir("sortie"):
+        img = preprocess_image("sortie/" + i)
+        b += model.predict(img)[0][0]
+        c += 1
+    
+    if b/c > 0.55:
+        return 1
+    else:
+        return 0
+
+print(socket.gethostbyname(socket.gethostname()))
+
+
+async def serv(a):
+    with open("video.mp4", "wb") as f:
+        f.write(a)
+    decouper_video_en_images("video.mp4", "sortie")
+    x = verifie()
+    return x
+
+async def echo(websocket, path):
+    async for message in websocket:
+        await websocket.send(serv(message))
+
+async def main():
+    async with websockets.serve(echo, socket.gethostbyname(socket.gethostname()), 8765):
+        await asyncio.Future() 
+
+asyncio.run(main())
+print("fin")
